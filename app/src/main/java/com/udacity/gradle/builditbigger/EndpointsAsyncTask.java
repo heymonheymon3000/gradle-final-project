744e8f6e3836b184397740320031356e72c99203
heymonheymon3000@yahoo.com <terry.parrish@emory.edu>
2018-07-11 08:46:48 -0400

    Clean up

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -31,7 +31,7 @@ public class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - 10.0.3.2 is localhost's IP address in Genymotion emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.3.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?>
--- jokes-lib/src/main/java/com/tparri/jokes/JokesDataSource.java
+++ jokes-lib/src/main/java/com/tparri/jokes/JokesDataSource.java
@@ -1,10 +1,6 @@
 package com.tparri.jokes;
 
 public class JokesDataSource {
-    private String joke;
-
-    public JokesDataSource(){}
-
     public String getJoke() {
         return "Why did the chicken cross the road? Hahaha";
     }
